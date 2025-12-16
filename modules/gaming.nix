ad03f986a42a4b58c122b729fc7c061b71ef43e4
0x0D <0xOD@proton.me>
2025-12-16 19:40:19 +0100

    Idk

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -4,20 +4,18 @@
     nix-gaming.nixosModules.platformOptimizations
   ];
 
-  config = {
-    programs.steam = {
-      enable = true;
-      package = pkgs.steam;
-      gamescopeSession.enable = true;
-      protontricks.enable = true;
-      extest.enable = true;
-    };
-
-    services.pipewire.lowLatency.enable = true;
-    programs.steam.platformOptimizations.enable = true;
-
-    programs.gamemode.enable = true;
-
-    networking.mihoyo-telemetry.block = true;
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam;
+    gamescopeSession.enable = true;
+    protontricks.enable = true;
+    extest.enable = true;
   };
+
+  services.pipewire.lowLatency.enable = true;
+  programs.steam.platformOptimizations.enable = true;
+
+  programs.gamemode.enable = true;
+
+  networking.mihoyo-telemetry.block = true;
 }
