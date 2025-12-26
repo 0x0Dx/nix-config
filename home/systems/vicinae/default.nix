55997f7b81f2cebb5b91ca462a932dc651cc7f63
0x0D <0xOD@proton.me>
2025-12-26 22:21:36 +0100

    fix: add additional command to exec-once for Hyprland window manager

--- home/systems/vicinae/default.nix
+++ home/systems/vicinae/default.nix
@@ -1,5 +1,8 @@
 { inputs, ... }: {
   imports = [inputs.vicinae.homeManagerModules.default];
   services.vicinae.enable = true;
-  wayland.windowManager.hyprland.settings.exec-once = ["systemctl enable --now --user vicinae.service"];
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "systemctl enable --now --user vicinae.service"
+    "vicinae server &"
+  ];
 }
